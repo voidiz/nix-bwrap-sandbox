cf3930b0b96e113d9e0f559c1fa2fe313938ba53
voidiz <29259387+voidiz@users.noreply.github.com>
2026-09-03 21:31:50 +0000

    naming

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     apps = forAllSystems (system: let
       pkg = self.packages.${system}.bwrap-sandbox;
     in {
-      nbs = {
+      bwrap-sandbox = {
         type = "app";
         program = "${pkg}/bin/bwrap-sandbox";
       };
